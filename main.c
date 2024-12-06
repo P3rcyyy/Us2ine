92b4115db9b73c85807b72b16bf95a3387e00d91
Noé Hutin <noe.hutin@ig2i.centralelille.fr>
2024-12-06 14:46:48 +0100

    init

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <sys/shm.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <stdio.h>
+#include "pthread.h"
+#include <string.h>
+#include <unistd.h>
+#include <semaphore.h> 
+
+int main (int argc, char *argv[]){
+
+
+
+    return 0;
+}
